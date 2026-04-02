b70047c803e8119143c27a5b9c53c42971c2d550
Your Name <you@example.com>
2026-04-02 10:47:26 +0700

    create app structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Suspense } from 'react';
+import { ErrorBoundary } from '@suspensive/react';
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary fallback={
+      <div className="fixed inset-0 flex flex-col items-center justify-center bg-white-bg z-[9999]">
+        <div className="p-4 text-red-600">Ошибка загрузки приложения</div>
+      </div>
+    }>
+      <Suspense fallback={
+        <div className="fixed inset-0 flex flex-col items-center justify-center bg-white-bg z-[9999]">
+          <div className="w-[50px] h-[50px] border-4 border-white-bg border-t-primary rounded-full animate-spin mb-4" />
+            <div className="text-sm text-gray">
+              Загрузка...
+            </div>
+        </div>
+      }>
+        <App />
+      </Suspense>
+    </ErrorBoundary>
   </StrictMode>,
 )
