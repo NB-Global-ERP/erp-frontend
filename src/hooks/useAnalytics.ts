f6c7ab6c5d31e674423651c501aee24190bebd26
Your Name <you@example.com>
2026-04-04 23:26:48 +0700

    fix

--- src/hooks/useAnalytics.ts
+++ src/hooks/useAnalytics.ts
@@ -5,6 +5,19 @@ export function useAnalytics() {
     const analytics = useERPStore((state) => state.analytics);
     const isLoading = useERPStore((state) => state.isLoading);
 
+    console.log({
+        basicStats: analytics.courseBasicStats,
+
+        totalCourses: analytics.courseCount,
+        totalCompanies: analytics.totalCompanies,
+        totalEmployees: analytics.totalEmployees,
+        totalGroups: analytics.totalGroups,
+        totalSpecifications: analytics.totalSpecifications,
+        averageGroupProgress: analytics.averageGroupProgress,
+        totalRevenue: analytics.totalRevenue,
+
+    })
+
     return useMemo(() => ({
         basicStats: analytics.courseBasicStats,
 
